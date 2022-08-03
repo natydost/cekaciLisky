8194043f12a565e8f2c8c89c49c1edefac68a57b
Julia Melik <julija.melik@gmail.com>
2022-08-03 14:34:09 +0200

    JuliaMelik klientské čislo

---
+++ script.js
@@ -0,0 +1,6 @@
+let client = new Paho.MQTT.Client(
+    'd57a0d1c39d54550b147b58411d86743.s2.eu.hivemq.cloud',
+    8884,
+    'printgo' + Math.random()
+)
+document.getElementById("demo").innerHTML = client;
